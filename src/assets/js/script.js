dff6c8ac819b8f81f6f33ef8c57bef8d872209f7
Mauro Pfister <mauro.pfister@epfl.ch>
2021-10-18 11:52:04 +0200

    Cleanup old css and js files

--- src/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-/* Open and close mobile navigation */
-document.querySelector('.nav__toggle').addEventListener("click", (e) => { 
-
-    document.querySelector('.nav').classList.toggle('nav--open');
-    document.querySelector('.hamburger').classList.toggle('hamburger--open');
-    document.querySelector('body').classList.toggle('no-scroll');
-});
