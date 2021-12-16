e11e353036e61f7f1ab474de13a5b5441e2c684e
Mauro Pfister <mauro.pfister@epfl.ch>
2021-12-16 01:25:11 +0100

    Add responsive images to project pages

--- src/utils/shortcodes.js
+++ src/utils/shortcodes.js
@@ -1,3 +1,4 @@
+BASE_URL_IMG = "https://res.cloudinary.com/mauropfister/image/upload/"
 // Shortcode for embedding vimeo video
 const vimeo = function (videoURL, aspectRatio, title = '') {
     const url = new URL(videoURL);
@@ -13,14 +14,32 @@ const vimeo = function (videoURL, aspectRatio, title = '') {
     `;
 };
 
-// Shortcode for responsive images
-const respimg = function (image) {
+function getSrcset(image, widths) {
+    return widths.map(width => {
+      return `${getSrc(image, width)} ${width}w`;
+    }).join(", ")
+};
+
+function getSrc(image, width) {
+    return `${BASE_URL_IMG}q_auto,f_auto,w_${width}/${image}`
+};
+
+// Shortcode for responsive images using srcset
+// TODO: Is this the way to implement named arguments?
+const respimg = function (image, options = {}) {
+
+    const loading = options.loading ? options.loading : "eager"
+    const alt = options.alt ? options.alt : ""
+
+    const widths = [ 320, 640, 960, 1280, 1600 ];
 
     return `
     <img 
-        src="${image}"
-        srcset="${image} 320w, ${image} 800w, ${image} 1200w"
-        alt="" loading="lazy"
+        src="${getSrc(image, 640)}"
+        srcset="${getSrcset(image, widths)}""
+        sizes="(min-width: 62em) 50vw, 90vw"
+        alt="${alt}" 
+        loading="${loading}"
     ></img>
     `;
 };
